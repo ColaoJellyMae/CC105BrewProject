ba815efe5e4a69bf481a662f6900b6addd0848cb
ColaoJellyMae <bg201802353@wmsu.edu.ph>
2020-11-20 22:05:24 -1200

    3rd Video

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:brew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class  wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home(
+      
+    );
+  }
+}
