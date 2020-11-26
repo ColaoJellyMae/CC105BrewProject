8c554cddd833692c91c2e2e5ded8d7bf96b72aa0
ColaoJellyMae <bg201802353@wmsu.edu.ph>
2020-11-25 20:53:22 -1200

    16th video

---
+++ lib/Services/database.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseSService {
+  //collection reference
+  final CollectionReference brewCollection =
+  
+  //created directly behind the scene (Firestore -> FirebaseFirestore)
+      FirebaseFirestore.instance.collection('brews');
+}
