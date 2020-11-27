8341b56fea1e44357c9f1fc787080be75146095b
ColaoJellyMae <bg201802353@wmsu.edu.ph>
2020-11-26 16:20:11 -1200

    23rd video Slider

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -46,6 +46,15 @@ class _SettingsFormState extends State<SettingsForm> {
           ),
 
           //slider
+          Slider(
+            value:(_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            inactiveColor: Colors.brown[_currentStrength ?? 100],
+            min:100,
+            max:900,
+            divisions: 8,
+            onChanged: (val) => setState(() => _currentStrength =val.round()),
+          ),
           RaisedButton(
               color: Colors.black,
               child: Text(
